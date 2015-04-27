99508cd4debbce0e4027169d4971c1144b354aa3
iLStAx <marco.salinas.12@sansano.usm.cl>
2015-04-27 11:43:02 -0300

    Taylor y Taylor 2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import math as ma
+import numpy as np
 from biseccion import *
 from newton import *
 #import newton2.py
 from puntofijo import *
 from secante import *
 from helper import *
+import matplotlib.pyplot as plt
+# from sympy import *
 
 def f1(x):
 	return x**2-6
@@ -17,6 +20,40 @@ def f3(x):
 
 def f4(x):
 	return ma.e**(x**2)*x
+
+def f5(x):
+	return ma.cos(x)
+
+def Taylor(f,x,x0):
+ 	return ma.acos(((f(x) - f(x0) - dercos(x0,1)*(x-x0) )*2)/((x-x0)**2))
+
+# def derivada(f,n):
+# 	return diff(cos(x),x,n) CON LIBRERIA SYMPY DERIVADA ENESIMA EN X DE COS(X)
+
+def dercos(x,n):
+	if n == 1:
+		return -ma.sin(x)
+	elif n == 2:
+		return -ma.cos(x)
+
+def Taylor2():
+	s = np.arange(-0.75,0.8,0.1)
+	x = s.tolist() 
+
+	for i in range (0,len(x)):
+		x[i] = round(x[i],2)
+
+	x.append(0.8)
+	y = [0]*len(x)
+
+	for i in range(0,len(x)):
+		y[i] = Taylor(f5,x[i],ma.pi/2)
+
+	plt.plot(x,y)
+	plt.ylabel("C(x)")
+	plt.xlabel("X")
+	plt.title("Expansion de Taylor C(X)")
+	plt.show()
 
 input1 = "x^{2} -6"
 g1 = ma.sqrt(6)
@@ -31,6 +68,8 @@ print "Resultado biseccion: "+str(Biseccion(f1, 10**(-5),-2,3))
 print "Resultado biseccion: "+str(Biseccion(f2, 10**(-5),0.5,1.5))
 print "Resultado biseccion: "+str(Biseccion(f3, 10**(-5),0,4))
 print "Resultado biseccion: "+str(Biseccion(f4, 10**(-5), 1.0, 2.0))
+print "Resultado C Taylor: " +str(Taylor(f5,ma.pi,ma.pi/2))
+print Taylor2()
 #PuntoFijo(g1,0.0,10**(-3))
 #PuntoFijo(g2,2.0,10**(-3))
 #PuntoFijo(g3,2.5,10**(-3))
