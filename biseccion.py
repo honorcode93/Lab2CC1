da25a857cdd225fc6d1d5763625e6f0598e50d75
iLStAx <marco.salinas.12@sansano.usm.cl>
2015-04-27 00:39:51 -0300

    biseccion 1,2,3 ok

--- biseccion.py
+++ biseccion.py
@@ -5,17 +5,6 @@ input1 = "-2x^{2} +6"
 input2 = "x^{5} -5x^{2} +4"
 input3 = "x^{4} -6x^{3} +12x^{2} -10x +3"
 
-def biseccion(tol,a,b):
-    while ((b-a)/2) > tol:
-        c = (a+b)/2
-        if f1(c)==0:
-            break
-        if f1(a)*f1(c)<0:
-            b=c
-        else:
-            a=c
-    print c
-    return c       
 
 def feval(fun,a): #evalua la fucniona fun en a
 
@@ -27,13 +16,13 @@ def feval(fun,a): #evalua la fucniona fun en a
         if xs:
             x = xs.group(0).split("x")
             if x[0] == "":
-                result += a**int(xs.group(3))
+                result += a**float(xs.group(3))
             elif x[0] == "-":  
-                result += -a**int(xs.group(3))
+                result += -a**float(xs.group(3))
             else:
-                result += int(x[0])*a**int(xs.group(3))
+                result += float(x[0])*a**float(xs.group(3))
         elif xj:
-            result += int(x[0])*a
+            result += float(x[0])*a
 
         else:
             if len(funcion[i].split("+")) > 1:
@@ -41,18 +30,33 @@ def feval(fun,a): #evalua la fucniona fun en a
                 if xsub:
                     x2 = xsub.group(0).split("x")
                     if x2[0] == "":
-                        result += a**int(xsub.group(3))
+                        result += a**float(xsub.group(3))
                     elif x2[0] == "-":  
-                        result += -a**int(xsub.group(3))
+                        result += -a**float(xsub.group(3))
                     else:
-                        result += int(x[0])*a**int(xsub.group())
+                        result += float(x[0])*a**float(xsub.group())
                 else:
-                    result += int(funcion[i].split("+")[1])
+                    result += float(funcion[i].split("+")[1])
             else:
-                result += int(funcion[i])
-
-    print result
+                result += float(funcion[i])
+    # print result
     return result
 
+def biseccion(fun,tol,a,b):
+    while ((b-a)/2) > tol:
+        c = (a+b)/2
+        if feval(fun,c)==0:
+            print c
+            return c
+        if feval(fun,a)*feval(fun,c)<0:
+            b=c
+        else:
+            a=c
+    print float(c)
+    return float(c)       
 
-feval(input2,2)
+feval(input2,3)
+
+biseccion(input1,10**(-5),-2,3)
+biseccion(input2,10**(-5),0.5,1.5)
+biseccion(input3,10**(-5),0,4)
