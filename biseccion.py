1e0f16d26cfc893f1a0fdce2e4ec7911b172688d
iLStAx <marco.salinas.12@sansano.usm.cl>
2015-04-27 01:18:20 -0300

    punto fijo 1,2,3 ok (creo que el 3 falla algo, nose que)

--- biseccion.py
+++ biseccion.py
@@ -1,10 +1,12 @@
 import re
 import math as ma
 
-input1 = "-2x^{2} +6"
+input1 = "x^{2} -6"
+g1 = ma.sqrt(6)
 input2 = "x^{5} -5x^{2} +4"
+g2 = "5x^{2} -4"
 input3 = "x^{4} -6x^{3} +12x^{2} -10x +3"
-
+g3 = "6x^{3} -12x^{2} +10x -3"
 
 def feval(fun,a): #evalua la fucniona fun en a
 
@@ -55,8 +57,36 @@ def biseccion(fun,tol,a,b):
     print float(c)
     return float(c)       
 
-feval(input2,3)
+def PuntoFijo(gx,x0,TOL):
+    i = 1
+    ini = x0
+    xi = 0
+    exp = 1
+    if isinstance(gx,float):
+        xi = gx
+    elif len(gx.split(" ")) == 2:
+        xi = feval(gx,ini)**(1/5)
+        exp = 1/5
+    elif len(gx.split(" ")) > 2:
+        xi = feval(gx,ini)**(1/4)
+        exp = 1/4
+    while i <= 100:
+        if exp == 1:
+            print gx
+            return gx
+        else:
+            xi = feval(gx,ini)**exp
+        dif = ma.fabs(ini-xi)
+        if dif<TOL:
+            print xi
+            return xi 
+        i = i+1
+        ini = xi
+    return False
 
 biseccion(input1,10**(-5),-2,3)
 biseccion(input2,10**(-5),0.5,1.5)
 biseccion(input3,10**(-5),0,4)
+PuntoFijo(g1,0.0,10**(-3))
+PuntoFijo(g2,2.0,10**(-3))
+PuntoFijo(g3,2.5,10**(-3))
